36411de159af72bddbaae3abaacc7bfccc951894
Viktors Makreckis <vmakreckis2809@gmail.com>
2026-07-01 19:30:10 +0100

    chore(init): initialize new sv project

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
+import adapter from '@sveltejs/adapter-node';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+			adapter: adapter(),
+			typescript: {
+				config: (config) => ({
+					...config,
+					include: [...config.include, '../drizzle.config.ts']
+				})
+			}
+		}),
+		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
+	],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
+});
